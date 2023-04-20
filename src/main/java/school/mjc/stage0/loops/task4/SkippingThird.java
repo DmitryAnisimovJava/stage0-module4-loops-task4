2d22aacbd3b08033aaf0bb83c5df6bc57ecce7e0
d.anisimov <d.anisimov@ds-manager03.szspspb.local>
2023-04-20 19:34:38 +0300

    S0M4.2T4 fixed.

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -4,13 +4,15 @@ package school.mjc.stage0.loops.task4;
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
     	int counter = 1;
-    	for (int numbers = 1; numbers != lastPrinted; numbers = lastPrinted > 0 ? ++numbers : --numbers) {
-    		if (counter == 3) {
-				counter = 1;
-				continue;
-			}
-    		System.out.println(numbers);
-    		counter++;
-    	}
+    	if (lastPrinted >= 1) {
+    		for (int numbers = 1; numbers <= lastPrinted; numbers++) {
+        		if (counter == 3) {
+    				counter = 1;
+    				continue;
+    			}
+        		System.out.println(numbers);
+        		counter++;
+        	}
+		}
     }
 }
